8fd48458ac4433e348ce2481fe482d80221e0829
Alfonso <Alfonso@leapwithalice.io>
2018-05-08 13:53:33 -0400

    New Site Update

--- form/contact.php
+++ form/contact.php
@@ -6,10 +6,10 @@ $mail = new PHPMailer();
 $emailTO = $emailBCC =  $emailCC = array(); $formEmail = '';
 
 ### Enter Your Sitename 
-$sitename = 'Leap With Alice';
+$sitename = 'Softnio';
 
 ### Enter your email addresses: @required
-$emailTO[] = array( 'email' => 'Alfonso@leapwithalice.io', 'name' => 'Alfonso' ); 
+$emailTO[] = array( 'email' => 'email@yoursite.com', 'name' => 'Your Name' ); 
 
 ### Enable bellow parameters & update your BCC email if require.
 //$emailBCC[] = array( 'email' => 'email@yoursite.com', 'name' => 'Your Name' );
